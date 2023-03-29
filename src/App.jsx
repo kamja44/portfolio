0e0551632e638a2701dbf4d8c466a524fde6c737
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-03-29 17:59:29 +0900

    Content가 들어갈 영역 설정

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,15 @@ const Wrap = styled(motion.div)`
   width: 100%;
   height: 100%;
   background-color: ${(props) => props.theme.bgColor};
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
 `;
 const DarkLightButton = styled(motion.div)`
   cursor: pointer;
+`;
+const Content = styled(motion.div)`
+  border: 1px soild black;
 `;
 function App() {
   const isDark = useRecoilValue(isDarkAtom);
@@ -30,7 +36,15 @@ function App() {
   return (
     <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
       <Wrapper>
-        <Wrap></Wrap>
+        <Wrap>
+          <Content />
+          <Content />
+          <Content />
+          <Content />
+          <Content />
+          <Content />
+        </Wrap>
+        {/* 추후 DarkLightButton은 footer로 빼기 */}
         <DarkLightButton onClick={toggleDarkAtom}>
           {isDark ? (
             <FontAwesomeIcon icon={faSun} size="2x" />
