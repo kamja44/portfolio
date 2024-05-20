5f2748570d78626883d65124a4d5c77ba1f0f020
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-05-20 23:24:32 +0900

    1차배포

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,83 +4,56 @@ export const projects = [
     title: "세계자연유산 모니터링 시스템",
     description: "HTML, CSS, JavaScript(ES5),Github",
     stack: "Front-End",
-    image: `${process.env.PUBLIC_URL}/image/제주시.jpg`, // 이미지 경로 설정
+    image: `${process.env.PUBLIC_URL}/image/jeju.webp`, // 이미지 경로 설정
   },
   {
     id: 2,
     title: "제주도청 전기차 충전기 시스템",
     description: "JavaScript(ES6+), Spring, MariaDB, Node.JS",
     stack: "Full-Stack",
-    image: `${process.env.PUBLIC_URL}/image/제주시.jpg`, // 이미지 경로 설정
+    image: `${process.env.PUBLIC_URL}/image/jeju.webp`, // 이미지 경로 설정
   },
   {
     id: 3,
     title: "카카오톡 클론코딩",
     description: "HTML, CSS, JavaScript(ES6+), Github",
     stack: "Front-End",
-    image: `${process.env.PUBLIC_URL}/image/카카오톡.jpg`, // 이미지 경로 설정
+    image: `${process.env.PUBLIC_URL}/image/kakaotalk.webp`, // 이미지 경로 설정
   },
   {
     id: 4,
     title: "유튜브 클론코딩",
     description: "Node.JS, MongoDB, ES6, Express",
     stack: "Full-Stack",
-    image: `${process.env.PUBLIC_URL}/image/유튜브.png`, // 이미지 경로 설정
+    image: `${process.env.PUBLIC_URL}/image/youtube.webp`, // 이미지 경로 설정
   },
   {
     id: 5,
     title: "MBIT 테스트 페이지",
     description: "HTML, CSS, JavaScript(ES6+), Django",
     stack: "Full-Stack",
-    image: `${process.env.PUBLIC_URL}/image/나나양_3월_배경.png`, // 이미지 경로 설정
+    image: `${process.env.PUBLIC_URL}/image/mbit.webp`, // 이미지 경로 설정
   },
   {
     id: 6,
     title: "트위터(X) 클론코딩",
     description: "JavaScript(ES6+), ReactJS, Firebase",
     stack: "Full-Stack",
-    image: `${process.env.PUBLIC_URL}/image/나나양_3월_배경.png`, // 이미지 경로 설정
+    image: `${process.env.PUBLIC_URL}/image/twitter.webp`, // 이미지 경로 설정
   },
   {
     id: 7,
     title: "Jeju-deers 웹페이지",
     description: "TypeScript, ReactJS, Node.js, MongoDB, Express",
     stack: "Full-Stack",
-    image: `${process.env.PUBLIC_URL}/image/나나양_3월_배경.png`, // 이미지 경로 설정
+    image: `${process.env.PUBLIC_URL}/image/misike.webp`, // 이미지 경로 설정
   },
   {
     id: 8,
-    title: "코코아톡 클론코딩",
-    description: "HTML, CSS, Github",
-    stack: "Back-End",
-    image: `${process.env.PUBLIC_URL}/image/나나양_3월_배경.png`, // 이미지 경로 설정
-  },
-  {
-    id: 9,
-    title: "코코아톡 클론코딩",
-    description: "HTML, CSS, Github",
-    stack: "Back-End",
-    image: `${process.env.PUBLIC_URL}/image/나나양_3월_배경.png`, // 이미지 경로 설정
-  },
-  {
-    id: 10,
-    title: "코코아톡 클론코딩",
-    description: "HTML, CSS, Github",
-    stack: "Back-End",
-    image: `${process.env.PUBLIC_URL}/image/나나양_3월_배경.png`, // 이미지 경로 설정
-  },
-  {
-    id: 11,
-    title: "코코아톡 클론코딩",
-    description: "HTML, CSS, Github",
-    stack: "Back-End",
-    image: `${process.env.PUBLIC_URL}/image/나나양_3월_배경.png`, // 이미지 경로 설정
-  },
-  {
-    id: 12,
-    title: "코코아톡 클론코딩",
-    description: "HTML, CSS, Github",
-    stack: "Full-Stack",
-    image: `${process.env.PUBLIC_URL}/image/나나양_3월_배경.png`, // 이미지 경로 설정
+    title: "영화 웹 만들기",
+    description: "React, React-Router-Dom, Github",
+    stack: "Front-End",
+    image: `${process.env.PUBLIC_URL}/image/movie-app.webp`, // 이미지 경로 설정
+    url: `https://kamja44.github.io/movie-app/`,
   },
 ];
